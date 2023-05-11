7f5d1d5984f952d16e30c8ac5783ef03a73c7535
Zahid Abdullah <zaevanphy@gmail.com>
2023-05-11 05:15:48 +0300

    UPDATE

---
+++ src/content_option.js
@@ -0,0 +1,184 @@
+const logotext = "NoSweatSuperDry";
+const meta = {
+  title: "Zahid Abdullah",
+  description:
+    "I’m Zahid Abdullah a Software Developer working in Ecoaims Oy, Finland.",
+};
+
+const introdata = {
+  title: "I’m Zahid Abdullah",
+  animated: {
+    first: "A Programmer and Developer",
+    second: "A Physicist",
+    third: "A Gamer",
+  },
+  description:
+    "I am a Software Developer at Ecoaims oy and currently pursuing a degree in Information and Communication Technology at Riveria school in Joensuu. My work involves using JavaScript, Node JS, and React-native. I hold two M.Sc. degrees in Physics and Photonics, where I worked on designing and analyzing a Micro-grid polarizer. Additionally, I have 6 months of experience working in Optical and clean-room laboratories. I also have experience in managing warehouses for Posti (Post of Finland) for 2 years.",
+  your_img_url:
+    "https://lh3.googleusercontent.com/drive-viewer/AFGJ81qaDcISouVdZdL7x8N2IDpqUk0CgSwNVP4_gWP4rtqYSbirmStnKh7_WZLiIyn1o9JnUVlmRhG37syyIF1zkTUUH6xlww=s1600",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "I am working as a Software Developer in Ecoaims oy. I am doing my apprenticeship by taking a degree in Information and Communication Technology in Riveria school, Joensuu and working in Ecoaims oy. I have been working with JavaScript, Node JS, React-native when working in the company. In my early life, I have an extensive background in Physics. I have two M.Sc. in Physics and Photonics. In M.Sc. Photonics I have designed and analyzed a Micro-grid polarizer as my Thesis work. I have 6 months of working experience in Optical laboratory and clean-room laboratory. After graduation, I have also worked in Warehouses inbound and outbound systems of Posti (Post of Finland). I have 2 years of warehouse management experience by hand.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Software Developer",
+    where: "Ecoaims Oy.",
+    date: "2021",
+  },
+  {
+    jobtitle: "Warehouse Associate",
+    where: "Posti & Customs Warehouse",
+    date: "2019",
+  },
+  {
+    jobtitle: "Maintenance Specialist",
+    where: "Lassila & Tikanoja Oy.",
+    date: "2015",
+  },
+];
+
+const skills = [
+  {
+    name: "JavaScript",
+    value: 70,
+  },
+  {
+    name: "React",
+    value: 85,
+  },
+  {
+    name: "React-native",
+    value: 90,
+  },
+  {
+    name: "Android & IOS Development",
+    value: 66,
+  },
+];
+
+const services = [
+  {
+    title: "Web Designing",
+    description:
+      "Website for early stage Business and entrepreneurs. Web pages are designed to have responsive UI.",
+  },
+  {
+    title: "Mobile Apps (Android & IOS)",
+    description: "Android and ios App for simple tasks.",
+  },
+  {
+    title: "Personal computer assembly and troubleshooting.",
+    description:
+      "Fix and assemble personal computer and troubleshoot software issues.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "zaevanphy@gmail.com",
+  YOUR_FONE: "(358)45-2129141",
+  description: "Please feel free to leave a message for query and tasks. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/noSweatSuperDry",
+  facebook: "https://facebook.com/zaevan",
+  linkedin: "https://linkedin.com/zaevan88/",
+  twitter: "https://twitter.com/nosweatsuperdry",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
